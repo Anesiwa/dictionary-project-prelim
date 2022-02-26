f564ef15254031d279c45c172b47c710d5391b97
Anesiwa <89821512+Anesiwa@users.noreply.github.com>
2022-02-26 11:36:37 -0600

    Created a function to pull the synonyms of the word being searched.

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -8,8 +9,14 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>{definition.definition}</p>
-            <p>{definition.example}</p>
+            <p>
+              <strong>Definition:</strong>
+              {definition.definition}
+              <br />
+              <strong>Example:</strong>
+              <em>{definition.example}</em>
+              <Synonyms synonyms={definition.synonyms} />
+            </p>
           </div>
         );
       })}
